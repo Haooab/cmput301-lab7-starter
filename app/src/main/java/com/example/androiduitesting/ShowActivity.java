efecf2b143022df655411de8d1579141a9820bcf
Huihao Peng <huihao@ualberta.ca>
2026-02-27 08:51:10 -0700

    recommit

--- app/src/main/java/com/example/androiduitesting/ShowActivity.java
+++ app/src/main/java/com/example/androiduitesting/ShowActivity.java
@@ -1,4 +1,32 @@
 package com.example.androiduitesting;
 
-public class ShowActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShowActivity extends AppCompatActivity {
+    TextView cityName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+
+        cityName = findViewById(R.id.city_name);
+        String nameFromIntent = getIntent().getStringExtra("city_name");
+        if (nameFromIntent != null) {
+            cityName.setText(nameFromIntent);
+        }
+
+        final Button backButton = findViewById(R.id.button_back);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
 }
